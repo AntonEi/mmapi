cde75850fa10a7f03b6a1c6d0edf69ea61d14836
Anton Eriksson <antonake@icloud.com>
2024-04-21 14:16:33 +0000

    import django.shortcuts to polls/view

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework import generics, permissions
 from .models import Question, Choice
 from .serializers import QuestionSerializer, ChoiceSerializer
