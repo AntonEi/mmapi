0401effc78b725335cc5ba2405ba36344b1c2300
Anton Eriksson <antonake@icloud.com>
2024-04-23 15:55:57 +0000

    add user to the polls

--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    users = models.ManyToManyField(User, blank=True)
 
     def __str__(self):
         return self.choice_text
