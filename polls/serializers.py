f3be38a63a39c0305b8a950ecd7d62739711f143
Anton Eriksson <antonake@icloud.com>
2024-04-23 12:44:34 +0000

    remove the read only in the serializers & def create

--- polls/serializers.py
+++ polls/serializers.py
@@ -7,8 +7,15 @@ class ChoiceSerializer(serializers.ModelSerializer):
         fields = ['id', 'question', 'choice_text', 'votes']
 
 class QuestionSerializer(serializers.ModelSerializer):
-    choices = ChoiceSerializer(many=True, read_only=True)
+    choices = ChoiceSerializer(many=True)
 
     class Meta:
         model = Question
         fields = ['id', 'question_text', 'created_at', 'choices']
+
+    def create(self, validated_data):
+        choices_data = validated_data.pop('choices')
+        question = Question.objects.create(**validated_data)
+        for choice_data in choices_data:
+            Choice.objects.create(question=question, **choice_data)
+        return question
